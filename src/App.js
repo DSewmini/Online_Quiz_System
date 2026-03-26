9b2b60d418437dfc8c7480f1f532f3b166697001
adeesha2003 <41-bit-0055@kdu.ac.lk>
2026-03-26 22:53:22 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -6,9 +6,22 @@ import Quiz from './pages/Student - quizes';
 import HomePage from "./pages/homepage";
 import LecturerDashboard from "./pages/Lecturer - dashboard";
 import StudentSchedule from "./pages/Student - Schedule";
+<<<<<<< HEAD
 import StudentDashboard from "./pages/student - dashboard";
 import StudentNotice from "./pages/Student - Notice";
 
+=======
+import StudentDashboard from "/pages/Student - Dashboard";
+import ProfilePopup from "./components/ProfilePopup";
+import AdminCourse from "./pages/Admin - courses";
+
+export default function App() {
+  const hash = window.location.hash.replace("#", "").toLowerCase();
+  if (hash === "home") return <HomePage />;
+  if (hash === "student-schedule") return <StudentSchedule />;
+  if (hash === "student-dashboard") return <StudentDashboard />;
+  if (hash === "admin-course") return <AdminCourse />;
+>>>>>>> 9d5d191 (Admin - Courses Part Created)
 
 function App() {
   return (
